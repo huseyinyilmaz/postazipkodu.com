0a3894dbcb0c698634f3ef9aef0411d5446c9b86
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2012-03-08 23:55:15 +0200

    code changed to user backbone fetch methods

--- assets/main.js
+++ assets/main.js
@@ -1,121 +1,144 @@
-window.base = 'pkinfo/';
-window.cities_key = 'cities';
-
-window.templates = {
-    listTemplate :_.template($('#listTemplate').html()),
-    resultTemplate:_.template($('#resultTemplate').html())
-};
-
-window.loadingContent = $('#loadingTemplate').html();
-
-window.containers = {
-    city: $('#cityContainer'),
-    county: $('#countyContainer'),
-    district: $('#districtContainer'),
-    providence: $('#providenceContainer'),
-    result: $('#resultContainer')
-};
-///////////
-// UTILS //
-///////////
-/*
- * Gets key for file name creation and return file url
- */
-function getFileUrl(key){
-    return base + key + '.json';
-};
-
-function setClear(view){
-    view.setClear();
-    if(view.subModuleViewObj)
-	setClear(view.subModuleViewObj);
-}
-
-////////////
-// MODELS //
-////////////
-
-BaseModel  = Backbone.Model.extend({});
-BaseCollection = Backbone.Collection.extend({model:BaseModel});
-BaseView = Backbone.View.extend(
-{
-    tagName:'div',
-    events:{
-	'click a': 'onClick'
-    },
-    render:function(){
-	this.$el.html(templates.listTemplate({collection:this.collection}));
-    },
-    setLoading:function(){
-	this.$el.html(loadingContent);
-    },
-    clearSubModules:function(){
-	if(this.subModuleViewObj){
-	    this.subModuleViewObj.$el.html('');
-	    this.subModuleViewObj.clearSubModules();
-	}
-    },
-    onClick: function(event){
-	var id=event.target.id;
-	this.subModuleContainer.html(loadingContent);
-	this.subModuleViewObj && this.subModuleViewObj.clearSubModules();
-	this.$('li').removeClass('active');
-	$(event.target).parent().addClass('active');
-	containers.result.html('');
-	$.getJSON(getFileUrl(id),
-		  _.bind(function(data, textStatus, jqXHR){
-		      if(textStatus==='success'){
-			  this.subModuleCollectionObj = new BaseCollection(data);
-			  this.subModuleViewObj = new this.subModuleView(
-			      {collection:this.subModuleCollectionObj});
-			  this.subModuleContainer.children().remove();
-			  this.subModuleContainer.append(this.subModuleViewObj.el);		      
-			  this.subModuleViewObj.render();
-		      }
-		  },this));//getJSON
-    },
-});
-
-ProvidenceView = BaseView.extend({
-    onClick:function(event){
-	var id=event.target.id;
-	var providence = this.collection.find(function(e){return e.get('model_id')==id;});
-	this.$('li').removeClass('active');
-	$(event.target).parent().addClass('active');
-	containers.result.html(templates.resultTemplate({location:providence.get('code'),code:providence.get('value')}));
-    }
-});
-
-DistrictView = BaseView.extend({
-    subModuleView:ProvidenceView,
-    subModuleContainer: window.containers.providence
-});
-
-CountyView = BaseView.extend({
-    subModuleView:DistrictView,
-    subModuleContainer: window.containers.district,
-});
-
-CityView = BaseView.extend({
-    subModuleView:CountyView,
-    subModuleContainer: window.containers.county,
-});
-
-function init(){
-    containers.city.html(loadingContent);
-    $.getJSON(getFileUrl(cities_key),
-	      function(data, textStatus, jqXHR){
-		  if(textStatus==='success'){
-		      window.cities = new BaseCollection(data);
-		      window.citiesView = new CityView({collection:window.cities});
-		      containers.city.children().remove();
-		      containers.city.append(citiesView.el);		      
-		      window.citiesView.render();
-		  }
-	      });//getJSON
-}
-
+"use strict";
 $(function(){
-    init();
-});
+    //////////////////////
+    // CREATE NAMESPACE //
+    //////////////////////
+    window.postal =  {
 
+	base:'pkinfo/',
+	cities_key: 'cities',
+	loadingContent: $('#loadingTemplate').html(),
+
+	templates: {
+	    listTemplate :_.template($('#listTemplate').html()),
+	    resultTemplate:_.template($('#resultTemplate').html())},
+    
+	containers: {
+	    city: $('#cityContainer'),
+	    township: $('#townshipContainer'),
+	    providence: $('#providenceContainer'),
+	    district: $('#districtContainer'),
+	    result: $('#resultContainer')},
+    
+	getFileUrl: function getFileUrl(key){
+	    return this.base + key + '.json';
+	},
+
+	setClear: function(view){
+	    view.setClear();
+	    if(view.subModuleViewObj)
+		setClear(view.subModuleViewObj);
+	}
+    
+    };
+
+    ////////////
+    // MODELS //
+    ////////////
+
+    postal.BaseModel  = Backbone.Model.extend({});
+    postal.BaseCollection = Backbone.Collection.extend({model: postal.BaseModel});
+
+    postal.BaseView = Backbone.View.extend({
+	    tagName:'div',
+	    events:{
+		'click a': 'onClick'
+	    },
+
+	    initialize:function(options){
+		this.baseKey = options.baseKey;
+		console.log(this.container);
+		this.container.html(postal.loadingContent);
+		this.clearSubModules();
+		this.collection = new postal.BaseCollection();
+		this.collection.url= postal.getFileUrl(this.baseKey);
+		
+		this.collection.fetch({
+		    success:_.bind(function(models,xhr,options){
+			console.log('success');
+     			this.container.children().remove();
+     			this.container.append(this.el);
+			this.render();
+		    }, this),
+		    error:_.bind(function(models,xhr,options){
+			console.log('error');
+		    }, this)
+		});
+
+	    },
+
+	    render:function(){
+		this.$el.html(postal.templates.listTemplate({collection:this.collection,display:this.display}));
+	    },
+	    
+	    clearSubModules:function(){
+		if(this.subModuleViewObj){
+		    this.subModuleViewObj.$el.html('');
+		    this.subModuleViewObj.clearSubModules();
+		}
+	    },
+	    onClick: function(event){
+		var id=event.target.id;
+		this.subModuleViewObj = new this.subModuleView({baseKey:id});
+		
+		this.$('li').removeClass('active');
+		$(event.target).parent().addClass('active');
+		//empty results
+		postal.containers.result.html('');
+
+	    }
+	});
+
+    postal.DistrictView = postal.BaseView.extend({
+	display: 'Mahalle/Koy',
+	container: postal.containers.district,
+	onClick: function(event){
+	    var id=event.target.id;
+	    var district = this.collection.find(function(e){return e.get('model_id')==id;});
+	    this.$('li').removeClass('active');
+	    $(event.target).parent().addClass('active');
+	    postal.containers.result.html(postal.templates.resultTemplate({location:district.get('code'),code:district.get('value')}));
+	}
+    });
+    
+    postal.ProvidenceView = postal.BaseView.extend({
+	display: 'Bolge',
+	container: postal.containers.providence,
+	subModuleView: postal.DistrictView,
+	subModuleContainer: postal.containers.district
+    });
+    
+    postal.TownshipView = postal.BaseView.extend({
+	display: 'ilce',
+	container: postal.containers.township,
+	subModuleView: postal.ProvidenceView,
+	subModuleContainer: postal.containers.providence
+    });
+    
+    postal.CityView = postal.BaseView.extend({
+	display: 'Sehir',
+	container: postal.containers.city,
+	subModuleView: postal.TownshipView,
+	subModuleContainer: postal.containers.township
+    });
+
+
+    postal.citiesView = new postal.CityView({baseKey:'cities'});
+
+
+    ////////////////
+    // INITIALIZE //
+    ////////////////
+    // $.getJSON(postal.getFileUrl(postal.cities_key),
+    // 	      function(data, textStatus, jqXHR){
+    // 		  if(textStatus==='success'){
+    // 		      postal.cities = new postal.BaseCollection(data);
+    // 		      postal.citiesView = new postal.CityView({collection:postal.cities});
+    // 		      postal.containers.city.children().remove();
+    // 		      postal.containers.city.append(postal.citiesView.el);
+    // 		      postal.citiesView.render();
+    // 		  }
+    // 	      });
+
+    
+});
